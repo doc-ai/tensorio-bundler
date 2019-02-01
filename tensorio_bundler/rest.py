1c002476a44e515062975a2fb3613e7970587341
Neeraj Kashyap <neeraj@doc.ai>
2019-01-31 23:52:33 -0800

    Working implemenation of bundler REST API

--- tensorio_bundler/rest.py
+++ tensorio_bundler/rest.py
@@ -24,6 +24,15 @@ class BundleHandler:
     """
     Handler for bundle creation requests
     """
+
+    required_keys = {
+        'tflite_path',
+        'model_json_path',
+        'assets_path',
+        'bundle_name',
+        'bundle_output_path'
+    }
+
     def on_post(self, req, resp):
         """
         Accepts POST requests to create a tfbundle from:
@@ -49,37 +58,77 @@ class BundleHandler:
           exists there.
         + Responds with a status code of 422 if a SavedModel path is specified but the build flag is
           not set in the JSON body of the request.
+        + Responds with a status code of 422 if the build flag is set to true but a SavedModel path
+          is not specified in the JSON body of the request.
         + Responds with a status code of 409 if a SavedModel path is specified and the build flag is
           set to true, but if there is already a file at the specified TFLite path.
-        + Responds with a 404 if one or more of the following assets is not found:
+        + Responds with a 404 if one or more of the following is not found:
             + model.json
             + assets directory
             + TFlite binary if SavedModel binary not specified, SavedModel binary otherwise
-            + "build": true, if SavedModel binary is specified
         """
         # The following assignment automatically returns a 400 response code if the input is not
         # parseable JSON.
         request_body = req.media
-        expected_keys = {
-            'model_json',
-            'tflite_binary',
-            'bundle_name',
-            'bundle_output_path'
-        }
-        missing_keys = [key for key in expected_keys if key not in request_body]
+
+        missing_keys = [key for key in self.required_keys if key not in request_body]
         if len(missing_keys) > 0:
             message = 'Request body missing the following keys: {}'.format(
                 ', '.join(missing_keys)
             )
             raise falcon.HTTPBadRequest(message)
-
+        
+        if request_body.get('saved_model_dir') is not None and request_body.get('build') != True:
+            raise falcon.HTTPUnprocessableEntity(
+                description=(
+                    'ERROR: "build" must be set to true in request body if "saved_model_dir" is'
+                    'specified'
+                )
+            )
+        
+        if request_body.get('build') is True and request_body.get('saved_model_dir') is None:
+            raise falcon.HTTPUnprocessableEntity(
+                description=(
+                    'ERROR: "saved_model_dir" must be specified in request body if "build" is'
+                    'set to true'
+                )
+            )
+        
+        if request_body.get('build') is True:
+            try:
+                bundler.tflite_build_from_saved_model(
+                    request_body.get('saved_model_dir'),
+                    request_body.get('tflite_path')
+                )
+            except bundler.TFLiteFileExistsError as e:
+                raise falcon.HTTPConflict(description=str(e))
+            except bundler.SavedModelDirMisspecificationError as e:
+                raise falcon.HTTPNotFound(description=str(e))
+            except Exception as e:
+                raise falcon.HTTPInternalServerError(str(e))
+        
+        try:
+            outfile = bundler.tfbundle_build(
+                request_body.get('tflite_path'),
+                request_body.get('model_json_path'),
+                request_body.get('assets_path'),
+                request_body.get('bundle_name'),
+                request_body.get('bundle_output_path')
+            )
+        except bundler.ZippedTFBundleExistsError as e:
+            raise falcon.HTTPConflict(description=str(e))
+        except bundler.ZippedTFBundleMisspecificationError as e:
+            raise falcon.HTTPNotFound(description=str(e))
+        except Exception:
+            raise falcon.HTTPInternalServerError()
+        
         resp.status = falcon.HTTP_200
-        resp.body = 'ok'
+        resp.body = outfile
 
 api = falcon.API()
 
-ping = PingHandler()
-api.add_route('/ping', ping)
+ping_handler = PingHandler()
+api.add_route('/ping', ping_handler)
 
-bundler = BundleHandler()
-api.add_route('/bundle', bundler)
+bundle_handler = BundleHandler()
+api.add_route('/bundle', bundle_handler)
